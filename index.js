e840844f84f7da28e8e47264e127458fc56612e8
Aurea <fu.sakura@gmail.com>
2018-11-19 14:46:39 -0800

    add buttons with functionality

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+
+const displayTime = function displayTime(time) {
+
+
+  let hours = 0;
+  if ( $('.clock').hasClass('twelve-hour') ) {
+    hours = time.getHours() > 12 ? time.getHours() - 12 : time.getHours();
+
+  } else {
+    hours = time.getHours();
+  }
+
+
+  const mins = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+  const secs = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+  return hours + ':' + mins + ':' + secs;
+}
+
+const ticktocky = function ticktocky() {
+
+  const currentTime = new Date();
+  $('.clock').html( displayTime(currentTime) );
+
+}
+
+$(document).ready( () => {
+  ticktocky();
+  window.setInterval(ticktocky, 1000);
+
+  $('.colors button').click( function() {
+    const color = $(this).html().toLowerCase();
+
+    $('.clock').removeClass('white red blue green purple');
+    $('.clock').addClass(color);
+  });
+
+  $('.text-align button').click( function() {
+    const direction = $(this).html().toLowerCase();
+
+    $('.clock').removeClass('center left right');
+    $('.clock').addClass(direction);
+    });
+
+  $('.time-display button').click( function() {
+    $('.clock').toggleClass('twelve-hour');
+  });
+});
